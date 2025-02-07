8e48e857b66c97f1b854bd50115dab087692d319
Douglas Souza Silva 💻 <120577393+ddouglss@users.noreply.github.com>
2025-02-07 02:36:29 -0300

    Update Program.cs

--- MvcWebIdentityA/Program.cs
+++ MvcWebIdentityA/Program.cs
@@ -2,14 +2,8 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using MvcWebIdentityA.Context;
-<<<<<<< HEAD
 using MvcWebIdentityA.Services;
-=======
-<<<<<<< HEAD
 using MvcWebIdentityA.Services;
-=======
->>>>>>> 23dde132088ba3ad476ff30ee0c52779fe2cbcfb
->>>>>>> 6c115c5858492edbe398a904299ef310bb3f663b
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -39,10 +33,6 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
         options.SlidingExpiration = true;
     });
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 6c115c5858492edbe398a904299ef310bb3f663b
 builder.Services.AddAuthorization(options =>
 {
     options.AddPolicy("RequireUserAdminGerenteRole",
@@ -66,71 +56,39 @@ builder.Services.AddScoped<ISeedUserRoleInitial, SeedUsersRoleInitial>();
 
 builder.Services.AddScoped<ISeedUserClaimsInitial, SeedUsersClaimsInitial>();
 
-<<<<<<< HEAD
-=======
-
-=======
->>>>>>> 23dde132088ba3ad476ff30ee0c52779fe2cbcfb
->>>>>>> 6c115c5858492edbe398a904299ef310bb3f663b
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
->>>>>>> 23dde132088ba3ad476ff30ee0c52779fe2cbcfb
->>>>>>> 6c115c5858492edbe398a904299ef310bb3f663b
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseRouting();
 
-<<<<<<< HEAD
+await CriarPerfisUsuariosAsync(app);
 await CriarPerfisUsuariosAsync(app);
 
-=======
-<<<<<<< HEAD
-await CriarPerfisUsuariosAsync(app);
-
-=======
->>>>>>> 23dde132088ba3ad476ff30ee0c52779fe2cbcfb
->>>>>>> 6c115c5858492edbe398a904299ef310bb3f663b
 app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapStaticAssets();
 
 app.MapControllerRoute(
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 6c115c5858492edbe398a904299ef310bb3f663b
     name: "MinhaArea",
    pattern: "{area:exists}/{controller=Admin}/{action=Index}/{id?}");
 
 
 app.MapControllerRoute(
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 23dde132088ba3ad476ff30ee0c52779fe2cbcfb
->>>>>>> 6c115c5858492edbe398a904299ef310bb3f663b
+    
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
 app.Run();
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 6c115c5858492edbe398a904299ef310bb3f663b
 
 async Task CriarPerfisUsuariosAsync(WebApplication app)
 {
@@ -145,10 +103,7 @@ async Task CriarPerfisUsuariosAsync(WebApplication app)
         var service = scope.ServiceProvider.GetService<ISeedUserClaimsInitial>();
         await service.SeedUserClaimsAsync();
     }
-<<<<<<< HEAD
 }
-=======
+
 }
-=======
->>>>>>> 23dde132088ba3ad476ff30ee0c52779fe2cbcfb
->>>>>>> 6c115c5858492edbe398a904299ef310bb3f663b
+
